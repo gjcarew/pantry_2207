e2699cd6451350a484bf700491448b317cfa8afa
Gavin Carew <gjcarew@gmail.com>
2022-08-02 08:23:07 -0700

    Add ingredient class

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,2 +1,9 @@
 class Ingredient
+  attr_reader :name, :unit, :calories
+
+  def initialize(init_hash)
+    @name = init_hash[:name]
+    @unit = init_hash[:unit]
+    @calories = init_hash[:calories]
+  end
 end
