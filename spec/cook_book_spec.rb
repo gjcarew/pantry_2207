082580497575f40961b097c8f786a0320b1eef23
Gavin Carew <gjcarew@gmail.com>
2022-08-02 08:46:56 -0700

    Add cook_book_spec tests

---
+++ spec/cook_book_spec.rb
@@ -0,0 +1,31 @@
+require './lib/cook_book'
+
+RSpec.describe CookBook do
+  before :each do
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient1, 4)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    @recipe2 = Recipe.new("Cheese Burger")
+    @cookbook = CookBook.new
+  end
+
+  it 'exists' do
+    expect(@cookbook).to be_a CookBook
+  end
+
+  it 'has a list of recipes' do
+    expect(@cookbook.recipes).to be_an Array
+    expect(@cookbook.recipes).to be_empty
+  end
+
+  it 'can add recipes' do
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+
+    expect(@cookbook.recipes).to eq([@recipe1, @recipe2])
+  end
+
+end
