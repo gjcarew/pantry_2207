971261a707dd41084bd6836dd5b18f1d5716552f
Gavin Carew <gjcarew@gmail.com>
2022-08-02 08:54:40 -0700

    Add recipe class

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -1,7 +1,8 @@
 require './lib/recipe'
+require './lib/ingredient'
 
-Rspec.describe Recipe do
-  before :Each do
+RSpec.describe Recipe do
+  before :each do
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
     @recipe1 = Recipe.new("Mac and Cheese")
@@ -20,7 +21,7 @@ Rspec.describe Recipe do
     @recipe1.add_ingredient(@ingredient1, 2)
     @recipe1.add_ingredient(@ingredient1, 4)
     @recipe1.add_ingredient(@ingredient2, 8)
-    expect(@recipe1.ingredients_required).to eq {@ingredient1 => 6, @ingredient2 => 8}
+    expect(@recipe1.ingredients_required).to eq({@ingredient1=> 6, @ingredient2=> 8})
   end
 
   it 'has an ingredient list' do
