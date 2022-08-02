64c6876e642b2a25fef0ffb7161f98a1da564dfa
Gavin Carew <gjcarew@gmail.com>
2022-08-02 10:04:03 -0700

    Add tests to cook_book_spec for iteration 4

--- spec/cook_book_spec.rb
+++ spec/cook_book_spec.rb
@@ -59,5 +59,30 @@ RSpec.describe CookBook do
     end
   end
 
+  context 'Iteration 4' do
+    before :each do
+      @cookbook = CookBook.new
+      ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+      ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+      recipe1 = Recipe.new("Mac and Cheese")
+      recipe1.add_ingredient(ingredient1, 2)
+      recipe1.add_ingredient(ingredient2, 8)
+      ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+      ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 1})
+      recipe2 = Recipe.new("Burger")
+      recipe2.add_ingredient(ingredient3, 4)
+      recipe2.add_ingredient(ingredient4, 100)
+      @cookbook.add_recipe(recipe1)
+      @cookbook.add_recipe(recipe2)
+    end
 
+    it "has a printed date" do
+      allow(@cookbook).to receive(:date).and_return("04-22-2020")
+      expect(@cookbook.date).to eq("04-22-2020")
+    end
+
+    it "has a summary" do
+      expect(@cookbook.summary).to eq([{:name=>"Mac and Cheese", :details=>{:ingredients=>[{:ingredient=>"Macaroni", :amount=>"8 oz"}, {:ingredient=>"Cheese", :amount=>"2 C"}], :total_calories=>440}}, {:name=>"Burger", :details=>{:ingredients=>[{:ingredient=>"Ground Beef", :amount=>"4 oz"}, {:ingredient=>"Bun", :amount=>"100 g"}], :total_calories=>500}}])
+    end
+  end
 end
